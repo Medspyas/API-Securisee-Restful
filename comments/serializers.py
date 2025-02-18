02aa7ced92112532b4b4977441acc037fbaa73c9
assakali <assakali.mehdi@gmail.com>
2025-02-18 10:51:56 +0100

    Ajout du champ id dans issue pour pouvoir créer un comment avec l'issue en question

--- comments/serializers.py
+++ comments/serializers.py
@@ -6,9 +6,11 @@ from .models import Comment, Issue
 
 class CommentSerializer(serializers.ModelSerializer):
     created_time = serializers.DateTimeField(format="%d/%m/%Y %H:%M", read_only=True)
-    issue = serializers.PrimaryKeyRelatedField(queryset=Issue.objects.all())
+    issue = serializers.PrimaryKeyRelatedField(queryset=Issue.objects.all())       
     author = serializers.StringRelatedField()   
     class Meta:
         model = Comment
         fields = ['id','description', 'issue', 'author', 'created_time']
         read_only_fields = ['id', 'author', 'created_time']
+
+       
--- issues/serializers.py
+++ issues/serializers.py
@@ -7,5 +7,5 @@ class IssueSerializer(serializers.ModelSerializer):
     created_time = serializers.DateTimeField(format="%d/%m/%Y %H:%M", read_only=True)   
     class Meta:
         model = Issue
-        fields = ['title', 'description', 'priority', 'tag', 'status', 'assignee', 'project', 'author', 'created_time']
+        fields = ['id','title', 'description', 'priority', 'tag', 'status', 'assignee', 'project', 'author', 'created_time']
         read_only_fields = ['author', 'created_time']
